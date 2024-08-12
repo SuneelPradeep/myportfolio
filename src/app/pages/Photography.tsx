89ce2e15fcbf4b52e6bb9ce87288e0abd40a7a71
SuneelPradeep <ernestsolomoninzion@gmail.com>
2024-08-12 17:54:46 +0530

    portfolio photography

--- src/app/pages/Photography.tsx
+++ src/app/pages/Photography.tsx
@@ -10,14 +10,16 @@ const Photography = () => {
         <Heading icon={sectionList[Section.Photography].icon} title={sectionList[Section.Photography].title}  />
      
       <div className='mx-6 lg:mx-12 my-8 '>
-      {/* eslint-disable  */}
+     
         {/* <iframe src="//lightwidget.com/widgets/6506843ebeaa57f7af189f3d7f5d23cf.html" scrolling="no" allowtransparency="true" 
         className="lightwidget-widget" style={{width:'100%',border:0,overflow:'hidden'}}>
 
         </iframe> */}
         
         <iframe src="//lightwidget.com/widgets/f1e05bbf556b5d50819ddc4b16173409.html" scrolling="no"
-         allowtransparency="true"  className="lightwidget-widget" style={{width:'100%',border:0,overflow:'hidden'}}>
+         allowtransparency="true" // eslint-disable-line
+
+           className="lightwidget-widget" style={{width:'100%',border:0,overflow:'hidden'}}>
 
          </iframe>
        </div>
